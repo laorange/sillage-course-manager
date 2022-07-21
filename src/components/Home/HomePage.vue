cfae93e2cf8225ea2ad260278c897e3a763bc252
辣橙 <68316902+laorange@users.noreply.github.com>
2022-07-21 11:09:32 +0800

    feat(Home): 一个简单的首页

--- src/components/Home/HomePage.vue
+++ src/components/Home/HomePage.vue
@@ -1,15 +1,71 @@
 <script setup lang="ts">
 import {useStore} from "../../pinia/useStore";
+import {useRouter} from "vue-router";
+import config from "../../../package.json";
+import {computed, ref} from "vue";
 
 const store = useStore();
+const router = useRouter();
+
+const languageIndex = ref(0);
+
+const languageOptions = computed(() => ["简体中文"].concat(store.config.languages).map((language, index) => {
+  return {
+    value: index,
+    label: language,
+  };
+}));
+
+const handlers = {
+  toDocs() {
+    window.open("https://laorange.github.io/sillage-docs");
+  },
+  toConfig() {
+    router.push({name: "config"});
+  },
+};
 </script>
 
 <template>
   <main>
-    <h1>{{ store.config.tableName }}</h1>
+    <n-space :vertical="true" justify="center" align="center" size="large">
+      <h1>{{ store.config.tableName }}</h1>
+      <div class="version">v{{ config.version }}</div>
+
+      <div class="language-selector">
+        <n-select v-model:value="languageIndex" :options="languageOptions"/>
+      </div>
+
+      <n-button v-for="grade in store.grades"
+                :key="`grade${grade}`"
+                type="success" size="large"
+                @click="router.push({name:`course`, query:{grade}})">
+        {{ grade }}
+      </n-button>
+
+      <n-button type="info" @click="handlers.toDocs">使用说明</n-button>
+
+      <n-button type="warning" @click="handlers.toConfig">系统配置</n-button>
+    </n-space>
   </main>
 </template>
 
 <style scoped>
+main {
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 
+h1 {
+  margin-bottom: 0;
+}
+
+.version {
+}
+
+.language-selector {
+  min-width: 200px;
+}
 </style>
--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {useRoute} from "vue-router";
 
 const route = useRoute();
 
 const activeIndex = ref(route.name ?? "");
 
+const routeName = computed(() => route.name);
 </script>
 <template>
-  <van-tabbar v-model="activeIndex" placeholder route>
-    <van-tabbar-item icon="wap-home-o" name="home" to="/"></van-tabbar-item>
-  </van-tabbar>
+  <div v-show="routeName!=='home'">
+    <van-tabbar v-model="activeIndex" placeholder route>
+      <van-tabbar-item icon="wap-home-o" name="home" to="/"></van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <style scoped>
