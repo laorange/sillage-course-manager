8feaa75a339220f6c8b04f1c0b0ec5b364fc92d6
辣橙 <68316902+laorange@users.noreply.github.com>
2022-07-26 16:42:41 +0800

    perf(CourseEditor): 多年级适配、优化变更提示

---
+++ src/assets/ts/whetherTwoObjEqual.ts
@@ -0,0 +1,36 @@
+// 参考：https://blog.csdn.net/qq_25742631/article/details/105581625
+
+// 判断数据类型
+function getDataType(data: any): string {
+    const temp = Object.prototype.toString.call(data);
+    const type = temp.match(/\b\w+\b/g);
+    return type ? (type.length < 2) ? "Undefined" : type[1] : "Undefined";
+}
+
+// 判断两个对象的属性是否相同
+export function whetherTwoObjEqual(source: any, comparison: any): boolean {
+    const iterable = (data: any) => ["Object", "Array"].includes(getDataType(data));
+    if (!iterable(source)) {
+        throw new Error(`source should be a Object or Array , but got ${getDataType(source)}`);
+    }
+
+    if (getDataType(source) !== getDataType(comparison)) {
+        return true;
+    }
+
+    const sourceKeys = Object.keys(source);
+
+    const comparisonKeys = Object.keys({...source, ...comparison});
+
+    if (sourceKeys.length !== comparisonKeys.length) {
+        return true;
+    }
+
+    return comparisonKeys.some(key => {
+        if (iterable(source[key])) {
+            return whetherTwoObjEqual(source[key], comparison[key]);
+        } else {
+            return source[key] !== comparison[key];
+        }
+    });
+}
--- src/components/CourseTable/CourseEditor/CourseEditor.vue
+++ src/components/CourseTable/CourseEditor/CourseEditor.vue
@@ -3,25 +3,29 @@ import {Course} from "../../../assets/ts/types";
 import {computed, ref, watch} from "vue";
 import WeekSelector from "./WeekSelector.vue";
 import SituationEditor from "./SituationEditor.vue";
-import {zhCN, dateZhCN, SelectOption, useMessage} from "naive-ui";
+import {zhCN, dateZhCN, SelectOption, useMessage, useDialog} from "naive-ui";
 import getWeeksString from "../../../assets/ts/getWeeksString";
 import {useStore} from "../../../pinia/useStore";
 import {formatDate, getWeekAmountBetweenTwoDay} from "../../../assets/ts/datetimeUtils";
 import dayjs from "dayjs";
 import {isValidCourse} from "../../../assets/ts/courseToolkit";
 import {useRoute} from "vue-router";
+import {whetherTwoObjEqual} from "../../../assets/ts/whetherTwoObjEqual";
 
 const store = useStore();
-const message = useMessage();
 const route = useRoute();
+const message = useMessage();
+const dialog = useDialog();
 
 const props = defineProps<{ course: Course }>();
 
 const courseLocal = ref<Course>({
   ...JSON.parse(JSON.stringify(props.course)),
   lessonNum: store.editor.lessonNum,
-  grade: route.query.grade ?? "",
+  grade: (route.query.grade instanceof Array ? route.query.grade[0] : route.query.grade) ?? null,
 });
+
+const whetherChanged = computed<boolean>(() => whetherTwoObjEqual(courseLocal.value, props.course));
 
 const handlers = {
   submitSuccess() {
@@ -30,16 +34,32 @@ const handlers = {
   submitFail() {
     message.error("提交失败，请检查网络连接");
   },
+  thinkTwiceBecauseDataChanged(hook: () => any, content: string, title: string = "提示") {
+    if (whetherChanged.value) {
+      dialog.info({
+        title: title,
+        content: content,
+        positiveText: "确定",
+        negativeText: "取消",
+        onPositiveClick: () => hook(),
+      });
+    } else {
+      hook();
+    }
+  },
   restore() {
-    courseLocal.value = JSON.parse(JSON.stringify(props.course));
-    store.editor.mode = "none";
-    store.editor.show = false;
+    this.thinkTwiceBecauseDataChanged(() => {
+      courseLocal.value = JSON.parse(JSON.stringify(props.course));
+      store.editor.mode = "none";
+      store.editor.show = false;
+    }, "您在本页面所做的修改将会丢失，是否继续？");
   },
   async update() {
     if (!whetherCourseIsValid) {
       message.error("请将数据补充完整(红色边框代表必填项)");
     } else if (JSON.stringify(props.course) === JSON.stringify(courseLocal.value)) {
-      message.warning("没有发现任何更改~");
+      message.warning("因为没有更改，所以无事发生");
+      this.restore();
     } else {
       // 如有冲突，阻止 并弹出警告
       let conflict = store.getConflictOfCourse(courseLocal.value);
@@ -47,11 +67,13 @@ const handlers = {
         return message.error(conflict);
       }
 
-      store.client.Records.update("course", courseLocal.value.id, courseLocal.value).then(() => {
-        store.courses = store.courses.filter(c => c.id !== store.editor.courseEditing.id).concat(courseLocal.value);
-        store.editor.show = false;
-        handlers.submitSuccess();
-      }).catch(handlers.submitFail);
+      this.thinkTwiceBecauseDataChanged(() => {
+        store.client.Records.update("course", courseLocal.value.id, courseLocal.value).then(() => {
+          store.courses = store.courses.filter(c => c.id !== store.editor.courseEditing.id).concat(courseLocal.value);
+          store.editor.show = false;
+          handlers.submitSuccess();
+        }).catch(handlers.submitFail);
+      }, "将会把变更提交数据库，是否继续？");
     }
   },
   add() {
@@ -64,11 +86,13 @@ const handlers = {
         return message.error(conflict);
       }
 
-      store.client.Records.create("course", courseLocal.value).then((record) => {
-        store.courses.push(record as unknown as Course);
-        store.editor.show = false;
-        handlers.submitSuccess();
-      }).catch(handlers.submitFail);
+      this.thinkTwiceBecauseDataChanged(() => {
+        store.client.Records.create("course", courseLocal.value).then((record) => {
+          store.courses.push(record as unknown as Course);
+          store.editor.show = false;
+          handlers.submitSuccess();
+        }).catch(handlers.submitFail);
+      }, "将会把变更提交数据库，是否继续？");
     }
   },
 };
@@ -131,7 +155,7 @@ const whetherCourseIsValid = computed<boolean>(() => isValidCourse(courseLocal.v
     <div class="responsive-right-part">
       <div aria-label="年级（大组）">
         <n-divider :dashed="true">年级 / 大组</n-divider>
-        <n-select v-model:value="courseLocal.grade" :filterable="true" :tag="true" :disabled="store.editor.mode==='add'" :options="gradeOptions"/>
+        <n-select v-model:value="courseLocal.grade" :filterable="true" :tag="true" :options="gradeOptions"/>
       </div>
 
       <div aria-label="班级（小组）">
@@ -156,31 +180,16 @@ const whetherCourseIsValid = computed<boolean>(() => isValidCourse(courseLocal.v
   <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
     <n-space justify="center" align="center">
       <template v-if="store.editor.mode==='edit'">
-        <n-popconfirm @positive-click="handlers.update()">
-          <template #trigger>
-            <n-button type="success" :disabled="!whetherCourseIsValid">提交更改</n-button>
-          </template>
-          将会把变更提交数据库，是否继续？
-        </n-popconfirm>
+        <n-button type="success" :disabled="!whetherCourseIsValid" @click="handlers.update()">提交更改</n-button>
       </template>
 
       <template v-if="store.editor.mode==='add'">
-        <n-popconfirm @positive-click="handlers.add()">
-          <template #trigger>
-            <n-button type="success" :disabled="!whetherCourseIsValid">确认新增</n-button>
-          </template>
-          将会把变更提交数据库，是否继续？
-        </n-popconfirm>
+        <n-button type="success" :disabled="!whetherCourseIsValid" @click="handlers.add()">确认新增</n-button>
       </template>
 
       <div v-show="!whetherCourseIsValid">（已用<span style="color: red">红色</span>标注了<strong>未完成</strong>的必填项）</div>
 
-      <n-popconfirm @positive-click="handlers.restore()">
-        <template #trigger>
-          <n-button type="warning">取消修改</n-button>
-        </template>
-        您在本页面所做的修改将会丢失，是否继续？
-      </n-popconfirm>
+      <n-button type="warning" @click="handlers.restore()">取消修改</n-button>
 
       <n-button type="info" v-show="store.editor.mode==='add'" @click="store.editor.show=false">暂时退出</n-button>
     </n-space>
