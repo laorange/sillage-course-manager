6dd87c40263f1487eb5e7c47b92bda2b8c8f34db
辣橙 <68316902+laorange@users.noreply.github.com>
2022-08-03 18:16:13 +0800

    fix(PlanTable): showGrade缺失

--- src/components/PlanTable/PlanTable.vue
+++ src/components/PlanTable/PlanTable.vue
@@ -7,12 +7,13 @@ import PlanTableBody from "./PlanTableBody.vue";
 
 const store = useStore();
 
+const showGrade = ref<boolean>(false);
 const filteredCourses = ref<Course[]>([]);
 </script>
 
 <template>
   <h1>{{ store.translate(`教学计划`) }}</h1>
-  <RouteFilter v-model:courses="filteredCourses"/>
+  <RouteFilter v-model:courses="filteredCourses" v-model:show-grade="showGrade"/>
   <PlanTableBody :courses="filteredCourses"/>
 </template>
 
