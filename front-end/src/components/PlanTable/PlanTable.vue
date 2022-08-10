cd8b7d6b2089317d6c411fa900f810343f0845ec
辣橙 <68316902+laorange@users.noreply.github.com>
2022-08-10 19:00:09 +0800

    perf(router): 路由守卫 不选年级即全选

--- front-end/src/components/PlanTable/PlanTable.vue
+++ front-end/src/components/PlanTable/PlanTable.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {useStore} from "../../pinia/useStore";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {Course} from "../../assets/ts/types";
 import RouteFilter from "../CourseTable/RouteFilter/RouteFilter.vue";
 import PlanTableBody from "./PlanTableBody.vue";
@@ -8,8 +8,9 @@ import {ShareOutline} from "@vicons/ionicons5";
 
 const store = useStore();
 
-const showGrade = ref<boolean>(false);
-const filteredCourses = ref<Course[]>([]);
+const routeFilter = ref<typeof RouteFilter>();
+
+const filteredCourses = computed<Course[]>(() => routeFilter?.value?.courses ?? []);
 
 const planTableBody = ref<typeof PlanTableBody>();
 
@@ -25,7 +26,7 @@ const handlers = {
 
 <template>
   <h1>{{ store.translate(`教学计划`) }}</h1>
-  <RouteFilter v-model:courses="filteredCourses" v-model:show-grade="showGrade">
+  <RouteFilter ref="routeFilter">
     <template #button>
       <n-space justify="center" align="center">
         <n-button :dashed="true" color="#32647d" @click="handlers.downloadDataAsCSV()">
--- front-end/src/router/router.ts
+++ front-end/src/router/router.ts
@@ -1,9 +1,16 @@
-import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
+import {createRouter, createWebHashHistory, NavigationGuardNext, RouteLocationNormalized, RouteRecordRaw} from "vue-router";
 import HomePage from "../components/Home/HomePage.vue";
 import ConfigEditor from "../components/ConfigEditor/ConfigEditor.vue";
 import CourseTable from "../components/CourseTable/CourseTable.vue";
 import LoginPage from "../components/LoginPage/LoginPage.vue";
 import PlanTable from "../components/PlanTable/PlanTable.vue";
+import {useStore} from "../pinia/useStore";
+
+function noGradeToAllGrades(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
+    const store = useStore();
+    if (!to.query.grade) next({path: to.path, query: {...to.query, grade: store.grades}});
+    else next();
+}
 
 
 const routes: RouteRecordRaw[] = [
@@ -22,6 +29,7 @@ const routes: RouteRecordRaw[] = [
         path: "/course/",
         name: "course",
         component: CourseTable,
+        beforeEnter: noGradeToAllGrades,
     },
     {
         path: "/login/",
@@ -32,6 +40,7 @@ const routes: RouteRecordRaw[] = [
         path: "/plan/",
         name: "plan",
         component: PlanTable,
+        beforeEnter: noGradeToAllGrades,
     },
 ];
 
