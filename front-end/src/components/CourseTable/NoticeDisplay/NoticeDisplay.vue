63bb656b69bf1d222f40f0bbf0fda087994f1b95
辣橙 <68316902+laorange@users.noreply.github.com>
2022-08-28 15:51:58 +0800

    perf(NoticeDisplay): 将content有值的公告优先显示

--- front-end/src/components/CourseTable/NoticeDisplay/NoticeDisplay.vue
+++ front-end/src/components/CourseTable/NoticeDisplay/NoticeDisplay.vue
@@ -2,10 +2,25 @@
 import {Notice} from "../../../assets/ts/types";
 import NoticeCard from "./NoticeCard.vue";
 import {useStore} from "../../../pinia/useStore";
+import {computed} from "vue";
+import dayjs from "dayjs";
 
 const props = defineProps<{ notices: Notice[] }>();
 
 const store = useStore();
+
+const noticeWithBulletinFirst = computed<Notice[]>(() => {
+  return props.notices.slice().sort((n1, n2) => {
+    // 将有内容的公告放置在前面
+    if (!!n2.content && !n1.content) {
+      return 1;
+    } else if (!!n1.content && !n2.content) {
+      return -1;
+    } else {
+      return dayjs(n2.updated).isAfter(dayjs(n1.updated)) ? 1 : -1;
+    }
+  });
+});
 </script>
 
 <template>
@@ -15,7 +30,7 @@ const store = useStore();
     <h2 style="text-align: center">{{ store.translate(`公告`) }}</h2>
 
     <n-timeline>
-      <NoticeCard v-for="notice of notices" :key="`notice${notice.id}`" :notice="notice"/>
+      <NoticeCard v-for="notice of noticeWithBulletinFirst" :key="`notice${notice.id}`" :notice="notice"/>
     </n-timeline>
   </div>
 </template>
