fe80e87af9ac157df70405ebbadf7dc4a234ca71
辣橙 <68316902+laorange@users.noreply.github.com>
2022-09-18 16:30:38 +0800

    feat: CopyrightDiv.vue

---
+++ front-end/src/components/AboutPage/CopyrightDiv.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import packageJson from "../../../package.json";
+
+withDefaults(defineProps<{ projectUrl: string, license?: string; addition?: string }>(), {license: "AGPL-3.0"});
+</script>
+
+<template>
+  <div class="copyright-div">
+    <div>
+      <a :href="projectUrl">
+        v{{ packageJson.version }} | Released under {{ license }}
+      </a>
+    </div>
+    <div v-if="addition">{{ addition }}</div>
+    <div>Copyright © 2022 <a href="https://github.com/laorange">GitHub@laorange</a></div>
+  </div>
+</template>
+
+<style scoped>
+.copyright-div {
+  font-size: xx-small;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+a:link, a:visited {
+  text-decoration: none;
+  color: black;
+}
+</style>
