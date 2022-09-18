0cf471f2ea053bf7fb6d7a1b18b4c728b62fde55
辣橙 <68316902+laorange@users.noreply.github.com>
2022-09-18 18:08:39 +0800

    feat: 在底部标签栏新增"上次访问"

--- front-end/src/components/TabBar.vue
+++ front-end/src/components/TabBar.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {useRoute} from "vue-router";
+import {Star} from "@vicons/ionicons5";
 
 const route = useRoute();
 
 const activeIndex = ref(route.name ?? "");
 
+const lastVisitIconColor = computed(() => {
+  return route.name === "course" ? "#1989fa" : undefined;
+});
 </script>
 <template>
   <van-tabbar v-model="activeIndex" placeholder route>
     <van-tabbar-item icon="wap-home" name="home" to="/"></van-tabbar-item>
+    <van-tabbar-item icon="star" name="course" to="/last-visit/">
+      <template #icon="props">
+        <n-icon :color="lastVisitIconColor">
+          <Star/>
+        </n-icon>
+      </template>
+    </van-tabbar-item>
   </van-tabbar>
 </template>
 
