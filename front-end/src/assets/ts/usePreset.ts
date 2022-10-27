c7a6564d232fc4f7c49da0c83b3d805dfe4084fa
辣橙 <68316902+laorange@users.noreply.github.com>
2022-10-27 20:10:43 +0800

    chore: add usePreset

---
+++ front-end/src/assets/ts/usePreset.ts
@@ -0,0 +1,14 @@
+import {CourseInfo} from "./types";
+
+export const courseInfoArray: CourseInfo[] = [];
+
+export const teacherArray: string[] = [];
+
+export const roomArray: string[] = [];
+
+export const methodArray: string[] = [
+    "理论课",
+    "习题课",
+    "实验课",
+    "考试",
+];
